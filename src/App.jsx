ace5b37d5605dd9d324b240d5db9ea0ffcbb121b
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-26 14:51:33 +0300

    Edited vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext'; // Import useAuthContext
+import { useAuthContext } from './hooks/useAuthContext'; 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Home } from './pages/Home/Home';
@@ -13,71 +13,58 @@ import Logout from './components/Logout/Logout';
 import Upload from './pages/Upload/Upload';
 import BookParty from './pages/BookParty/BookParty';
 import Profile from './pages/Profile/Profile';
-// import Profile from './components/Profile/Profile';
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Galery from './pages/Gallery/Gallery';
+import Gallery from './pages/Gallery/Gallery';
 import EditImage from './pages/EditImage/EditImage';
 import Statistics from './pages/Statistics/Statistics';
 import TrackUserActivity from './components/TrackUserActivity/TrackUserActivity';
-
 
 function App() {
   const { user, authIsReady } = useAuthContext(); // Get user and authIsReady from context
 
   return (
     <BrowserRouter>
-    {authIsReady && <TrackUserActivity />}
+      {authIsReady && <TrackUserActivity />}
       <div className='app-container'>
-      <ToastContainer /> 
+        <ToastContainer />
         <Header />
         {authIsReady && ( // Check if auth is ready before rendering routes
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/gallery" element={<Galery />} />
+            <Route path="/gallery" element={<Gallery />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/terms" element={<Terms />} />
             <Route path="/book" element={<BookParty />} />
             <Route
-              path='/edit/:id'
-              element={!user ? (
-                <Navigate to="/" /> // Redirect to home if user is not logged in
-              ) : user.role !== 'admin' ? (
-                <Navigate to="/" /> // Redirect if user is not admin
-              ) : (
-                <EditImage />
-              )}
+              path="/edit/:id"
+              element={
+                user && user.role === 'admin' ? (
+                  <EditImage />
+                ) : (
+                  <Navigate to="/" /> // Redirect if user is not logged in or not admin
+                )
+              }
             />
             <Route
-              path='/upload'
-              element={!user ? (
-                <Navigate to="/" /> // Redirect to home if user is not logged in
-              ) : user.role !== 'admin' ? (
-                <Navigate to="/" /> // Redirect if user is not admin
-              ) : (
-                <Upload />
-              )}
+              path="/upload"
+              element={
+                user && user.role === 'admin' ? (
+                  <Upload />
+                ) : (
+                  <Navigate to="/" /> // Redirect if user is not logged in or not admin
+                )
+              }
             />
-             <Route
-              path='/upload'
-              element={!user ? (
-                <Navigate to="/" /> // Redirect to home if user is not logged in
-              ) : user.role !== 'admin' ? (
-                <Navigate to="/" /> // Redirect if user is not admin
-              ) : (
-                <Upload />
-              )}
-            />
-             <Route
-              path='/statistics'
-              element={!user ? (
-                <Navigate to="/" /> // Redirect to home if user is not logged in
-              ) : user.role !== 'admin' ? (
-                <Navigate to="/" /> // Redirect if user is not admin
-              ) : (
-                <Statistics />
-              )}
+            <Route
+              path="/statistics"
+              element={
+                user && user.role === 'admin' ? (
+                  <Statistics />
+                ) : (
+                  <Navigate to="/" /> // Redirect if user is not logged in or not admin
+                )
+              }
             />
             <Route
               path="/register"
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 import dotenv from 'dotenv';
+import { sitemap } from 'vite-plugin-sitemap';
+
+
 dotenv.config();
+
+// Define the base URL of your website for the sitemap
+const BASE_URL = process.env.VITE_BASE_URL || 'http://localhost:3000'; // Default to localhost if BASE_URL is not set
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      // Sitemap options
+      baseUrl: BASE_URL,
+      outDir: 'dist', // Output directory for the generated sitemap
+      routes: [
+        { path: '/', lastmod: new Date().toISOString() },
+        { path: '/gallery', lastmod: new Date().toISOString() },
+        { path: '/contacts', lastmod: new Date().toISOString() },
+        { path: '/terms', lastmod: new Date().toISOString() },
+        { path: '/book', lastmod: new Date().toISOString() },
+        { path: '/upload', lastmod: new Date().toISOString() },
+        { path: '/statistics', lastmod: new Date().toISOString() },
+        { path: '/register', lastmod: new Date().toISOString() },
+        { path: '/login', lastmod: new Date().toISOString() },
+        { path: '/logout', lastmod: new Date().toISOString() },
+        { path: '/profile', lastmod: new Date().toISOString() },
+        // Add other routes here
+      ],
+    }),
+  ],
+});
