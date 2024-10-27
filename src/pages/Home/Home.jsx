88ff69d0c2a204e9f2e2b6bdc558bb18216bd430
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-27 11:35:47 +0200

    Intro component edited

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import Intro from '../../components/Intro/Intro'
 import Slider from '../../components/Slider/Slider'
 import './Home.css'
+import { Helmet } from 'react-helmet';
 
 export const Home = () => {
     return (
         <div className='home-container'>
+            <Helmet>
+            <title>Начална страница - Friends детски парти център</title>
+            <meta name="description" content="Friends - детски парти център в Бургас, жк. Изгрев. Игри, почасови забавления, празненства и организирани рождени дни за малчугани на едно място!" />
+            </Helmet>
             <div className='home-component'>
 
             </div>
