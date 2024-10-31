8adad2d59f1fa13f84795f27be0661ec0d679bd1
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-31 13:04:37 +0200

    Intro edited

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,5 @@
 import './Intro.css';
-import hero1 from '../../assets/YaDQ-resize.gif'; 
+import hero1 from '../../assets/YaDQ-resize.gif';
 import hero2 from '../../assets/3F3F-resize.gif';
 import hero3 from '../../assets/3vIU-resize.gif';
 import hero4 from '../../assets/1V8t-reduced.gif';
@@ -10,13 +10,13 @@ export default function Intro() {
     return (
         <div className="intro-container">
             <div className="intro-text">
-                Добре дошли в Парти център Friends! 
+                Добре дошли при нас.
                 <img src={hero1} alt="Hero 1" className="hero-icon" />
-                Тук ще откриете забавни игри и безкрайни плиключения!
+                <h1>Парти център Friends предлага почасови игри и организиране на рождени дни.</h1>
                 <img src={hero2} alt="Hero 2" className="hero-icon" />
-                Почасови игри или парти по повод
+                Ще се радваме да прекарате свободното си време при нас.
                 <img src={hero3} alt="Hero 3" className="hero-icon" />
-                Ние ще се погрижим да се почувствате страхотно
+                <h1>Ние ще се погрижим да се почувствате страхотно</h1>
                 <img src={hero4} alt="Hero 4" className="hero-icon" />
                 За повече въпроси се свържете с нас чрез контактната ни форма
                 <img src={hero5} alt="Hero 5" className="hero-icon" />
